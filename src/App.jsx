54194a8e33f4a2d3d8147866f5fc623c7ecefdc8
Ryma <mr_felkir@esi.dz>
2024-08-04 16:55:54 +0100

    first page almost done2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import Footer from "./components/Footer"; // Adjust the path as needed
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Home />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
