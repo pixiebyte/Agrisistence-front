eaedc5c06caa55aa9f40f8cb7982b02b8989a6cd
Ryma <mr_felkir@esi.dz>
2024-08-04 14:34:43 +0100

    first page almost done2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import profile from "../images/profile.png";
 import improve from "../images/improve.png";
 import aim from "../images/aim.png";
 import manage from "../images/manage.png";
-import partner from "../images/partener.png";
+import partner from "../images/partner.png";
 
 const Home = () => {
   return (
@@ -16,11 +16,7 @@ const Home = () => {
           <Card className="text-center custom-card welcome-card">
             <Card.Body className="d-flex flex-column align-items-start p-3 position-relative">
               <div className="top-left position-absolute">
-                <img
-                  src={profile} // Ensure the path is correct
-                  alt="Logo"
-                  className="card-image"
-                />
+                <img src={profile} alt="Logo" className="card-image" />
               </div>
               <div className="welcome-text">
                 <h5>
@@ -38,13 +34,20 @@ const Home = () => {
         </Col>
 
         <Col md={6}>
-          <Card className="text-center custom-card">
-            <Card.Body>
-              <Card.Title className="text-green">Our Aim</Card.Title>
-              <Card.Text>
-                We aim to improve agriculture in Africa and exploit the lands
-                available for a better lifestyle and a better economy.
-              </Card.Text>
+          <Card className="text-center custom-card aim-card">
+            <Card.Body className="d-flex flex-column align-items-start">
+              <div className="aim-card-content">
+                <div className="aim-card-text">
+                  <Card.Title className="text-green">Our Aim</Card.Title>
+                  <Card.Text>
+                    We aim to improve agriculture in Africa and exploit the
+                    lands available for a better lifestyle and a better economy.
+                  </Card.Text>
+                </div>
+                <div className="aim-card-icon">
+                  <img src={aim} alt="Aim" />
+                </div>
+              </div>
             </Card.Body>
           </Card>
         </Col>
@@ -55,14 +58,14 @@ const Home = () => {
           <ServiceCard
             title={<span className="text-green">Help manage your land</span>}
             text="We offer tracking of the improvements and progress as well as daily improvements suggestions and reminders"
-            icon={<img src={manage} alt="Manage" />} // Ensure the path is correct
+            icon={<img src={manage} alt="Manage" />}
           />
         </Col>
         <Col xs={12} md={4} className="service-card-col mb-3">
           <ServiceCard
             title={<span className="text-green">Look for partners</span>}
             text="We help you find suitable business partners and build a consistent network for: product marketing, supply and promotion"
-            icon={<img src={partner} alt="Partners" />} // Ensure the path is correct
+            icon={<img src={partner} alt="Partners" />}
           />
         </Col>
         <Col xs={12} md={4} className="service-card-col mb-3">
@@ -71,7 +74,7 @@ const Home = () => {
               <span className="text-green">Improve financial profits</span>
             }
             text="We help you invest in promising products and ensure the highest profit, financial management and best business plan"
-            icon={<img src={improve} alt="Financial" />} // Ensure the path is correct
+            icon={<img src={improve} alt="Financial" />}
           />
         </Col>
       </Row>
